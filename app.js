7b5c284c5a7944d9a87f3932e98a1fb04229b9ca
Souhitya kar <souhityakar45@gmail.com>
2026-05-31 17:05:19 +0530

    add atlas

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use((req,res,next)=>{
 })
 
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/apnaghar");
+        await mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/apnaghar");
 }
 main()
 .then(()=>{
